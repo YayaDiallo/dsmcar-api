6dca3821e388162afa520ac9c755c45dae822997
Yaya Diallo <yayazdiallo@outlook.fr>
2024-12-24 09:58:35 +0100

    feat: add express and and create the server

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 8080;
+
+app.get('/', (request: Request, response: Response) => {
+  response.json({ message: 'Welcome to dsmcar API', version: '1.0.0' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
