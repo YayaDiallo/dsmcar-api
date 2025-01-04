954ecab2eb0932018edbd93829ef8e06610dfd1f
Yaya Diallo <yayazdiallo@outlook.fr>
2025-01-04 19:21:27 +0100

    fix: rename drizzle folder to migrations and fetch schema from ./dist to avoid drizzle's <not found> error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
+  out: './migrations',
+  schema: './dist/db/schema/*.schema.js',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
