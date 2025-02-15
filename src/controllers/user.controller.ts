2372edb4cf666f2a917277643cef5f6e9e0f9b77
Yaya Diallo <yayazdiallo@outlook.fr>
2025-02-15 22:07:26 +0100

    feat: remove schema parsing from controller and unnecesary condition

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,7 @@
 import { Request, Response } from 'express';
 import { userService } from '../services/index.js';
 import { NotFound } from '../errors/index.js';
-import {
-  userInsertSchema,
-  userUpdateSchema,
-} from '../db/schema/user.schema.js';
+import { ParamsWithId } from '../interfaces/ParamsWithId.js';
 
 class UserController {
   private userService: typeof userService;
@@ -19,7 +16,6 @@ class UserController {
   }
 
   async create(request: Request, response: Response) {
-    userInsertSchema.parse(request.body);
     const [user] = await this.userService.create(request.body);
     response
       .status(201)
@@ -31,44 +27,32 @@ class UserController {
     const users = await this.userService.getAll();
     response.json(users);
   }
-  async getById(request: Request, response: Response) {
+  async getById(request: Request<ParamsWithId>, response: Response) {
     const { id } = request.params;
 
-    if (!id) {
-      response.status(400).json({ message: 'Missing user ID' });
-    } else {
-      const [user] = await this.userService.getById(id);
-      if (!user) {
-        throw new NotFound({
-          message: `User not found with id: \`${id}\``,
-          statusCode: 404,
-          code: 'ERR_NOT_FOUND',
-        });
-      }
-      response.json(user);
+    const [user] = await this.userService.getById(id);
+    if (!user) {
+      throw new NotFound({
+        message: `User not found with id: \`${id}\``,
+        statusCode: 404,
+        code: 'ERR_NOT_FOUND',
+      });
     }
+    response.json(user);
   }
 
-  async update(request: Request, response: Response) {
+  async update(request: Request<ParamsWithId>, response: Response) {
     const { id } = request.params;
-    userUpdateSchema.parse(request.body);
-    if (!id) {
-      response.status(400).json({ message: 'Missing user ID' });
-    } else {
-      const user = await this.userService.update(id, request.body);
-      response.status(200).json({ user });
-    }
+
+    const user = await this.userService.update(id, request.body);
+    response.status(200).json({ user });
   }
 
-  async delete(request: Request, response: Response) {
+  async delete(request: Request<ParamsWithId>, response: Response) {
     const { id } = request.params;
 
-    if (!id) {
-      response.status(400).json({ message: 'Missing user ID' });
-    } else {
-      await this.userService.delete(id);
-      response.status(204).send();
-    }
+    await this.userService.delete(id);
+    response.status(204).send();
   }
 }
 
