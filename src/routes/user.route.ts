9a3d21c67e8a62bd90b558178ec6120bc09f8b64
Yaya Diallo <yayazdiallo@outlook.fr>
2025-02-15 22:09:25 +0100

    feat: integrate validateRequest middleware to user routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,32 @@
 import express from 'express';
 import { userController } from '../controllers/index.js';
+import { validateRequest } from '../middlewares/index.js';
+import { ParamsWithId } from '../interfaces/ParamsWithId.js';
+import {
+  userInsertSchema,
+  userUpdateSchema,
+} from '../db/schema/user.schema.js';
 
 export const userRouter = express.Router();
 
 userRouter.get('/', userController.getAll);
-userRouter.get('/:id', userController.getById);
-userRouter.post('/', userController.create);
-userRouter.patch('/:id', userController.update);
-userRouter.delete('/:id', userController.delete);
+userRouter.get(
+  '/:id',
+  validateRequest({ params: ParamsWithId }),
+  userController.getById,
+);
+userRouter.post(
+  '/',
+  validateRequest({ body: userInsertSchema }),
+  userController.create,
+);
+userRouter.patch(
+  '/:id',
+  validateRequest({ params: ParamsWithId, body: userUpdateSchema }),
+  userController.update,
+);
+userRouter.delete(
+  '/:id',
+  validateRequest({ params: ParamsWithId }),
+  userController.delete,
+);
