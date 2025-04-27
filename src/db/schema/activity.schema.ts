bee38c71c7f3ee429b111b3a90cf0920fac8fea7
Yaya <yayazdiallo@outlook.fr>
2025-04-27 11:10:34 +0200

    feat: add activity schemas for insert, select, and update operations

--- src/db/schema/activity.schema.ts
+++ src/db/schema/activity.schema.ts
@@ -1,5 +1,11 @@
 import { usersTable } from '@/db/schema/user.schema.js';
 import { pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import {
+  createInsertSchema,
+  createSelectSchema,
+  createUpdateSchema,
+} from 'drizzle-zod';
+import { z } from 'zod';
 
 export const activitiesTable = pgTable('activities', {
   id: uuid().notNull().primaryKey().defaultRandom(),
@@ -11,3 +17,9 @@ export const activitiesTable = pgTable('activities', {
   createdAt: timestamp({ mode: 'string' }).notNull().defaultNow(),
   updatedAt: timestamp({ mode: 'string' }).notNull().defaultNow(),
 });
+
+export const activitySelectSchema = createSelectSchema(activitiesTable);
+export const activityInsertSchema = createInsertSchema(activitiesTable);
+export const activityUpdateSchema = createUpdateSchema(activitiesTable);
+
+export type ActivitySelectSchema = z.infer<typeof activitySelectSchema>;
