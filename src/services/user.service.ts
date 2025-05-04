ec3149c16a786b089c2bab6784a24e11d57d25df
Yaya <yayazdiallo@outlook.fr>
2025-05-04 10:30:22 +0200

    refactor: extend UserService from BaseService for improved data handling

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,50 +1,10 @@
-import { db } from '@/db/index.js';
-import { UserSelectSchema, usersTable } from '@/db/schema/user.schema.js';
-import { GetCollectionResponse } from '@/services/service.helper.js';
-import { eq, count } from 'drizzle-orm';
+import { UserInsertSchema, UserSelectSchema } from '@/db/schema/user.schema.js';
+import { userRepository } from '@/repositories/index.js';
+import { BaseService } from './base.service.js';
 
-class UserService {
-  async create(
-    userData: typeof usersTable.$inferInsert,
-  ): Promise<UserSelectSchema[]> {
-    const user = await db.insert(usersTable).values(userData).returning();
-    return user;
-  }
-  async getCollection(): Promise<GetCollectionResponse<UserSelectSchema>> {
-    const [rows, countResult] = await Promise.all([
-      db.select().from(usersTable),
-      db.select({ totalCount: count(usersTable.id) }).from(usersTable),
-    ]);
-
-    return {
-      totalCount: countResult[0]?.totalCount ?? 0,
-      rows,
-    };
-  }
-
-  async getById(id: string): Promise<UserSelectSchema | undefined> {
-    const [user] = await db
-      .select()
-      .from(usersTable)
-      .where(eq(usersTable.id, id));
-    return user;
-  }
-
-  async update(
-    id: string,
-    userData: typeof usersTable.$inferInsert,
-  ): Promise<{ id: string }[]> {
-    const user = await db
-      .update(usersTable)
-      .set({ ...userData })
-      .where(eq(usersTable.id, id))
-      .returning({ id: usersTable.id });
-
-    return user;
-  }
-
-  async delete(id: string): Promise<void> {
-    await db.delete(usersTable).where(eq(usersTable.id, id));
+class UserService extends BaseService<UserSelectSchema, UserInsertSchema> {
+  constructor() {
+    super(userRepository);
   }
 }
 
