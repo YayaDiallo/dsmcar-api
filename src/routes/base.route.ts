165479043ab7eda5d5264c933fe07a04bf4d0d78
Yaya <yayazdiallo@outlook.fr>
2025-05-07 08:31:05 +0200

    feat: implement BaseRoute class for structured route handling

---
+++ src/routes/base.route.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+
+export interface Controller {
+  path: string;
+}
+
+export abstract class BaseRoute<T extends Controller> {
+  protected controller: T;
+  public readonly router: express.Router;
+  protected path: string;
+
+  constructor({ controller }: { controller: T }) {
+    this.controller = controller;
+    this.path = controller.path;
+    this.router = express.Router();
+    this.initializeRoutes();
+  }
+
+  protected bindController(methodName: keyof T) {
+    const method = this.controller[methodName];
+    if (typeof method === 'function') {
+      return method.bind(this.controller);
+    }
+    throw new Error(
+      `Controller method ${String(methodName)} is not a function`,
+    );
+  }
+
+  protected abstract initializeRoutes(): void;
+}
