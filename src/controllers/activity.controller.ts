47bc0e3cbb04f55f55c11e1e63f1fafab7c5e685
Yaya <yayazdiallo@outlook.fr>
2025-05-08 17:20:01 +0200

    refactor: simplify ActivityController and CarController by extending BaseController

--- src/controllers/activity.controller.ts
+++ src/controllers/activity.controller.ts
@@ -1,57 +1,12 @@
-import { NotFound } from '@/errors/index.js';
-import { ParamsWithId } from '@/interfaces/index.js';
 import { activityService } from '@/services/index.js';
-import { Request, Response } from 'express';
+import { BaseController } from './base.controller.js';
 
-class ActivityController {
-  private activityService: typeof activityService;
-  private path: string;
-
+class ActivityController extends BaseController<typeof activityService> {
   constructor() {
-    this.activityService = activityService;
-    this.path = '/activities';
-    this.getCollection = this.getCollection.bind(this);
-    this.getById = this.getById.bind(this);
-    this.create = this.create.bind(this);
-    this.update = this.update.bind(this);
-    this.delete = this.delete.bind(this);
-  }
-
-  async create(request: Request, response: Response) {
-    const [activity] = await this.activityService.create(request.body);
-    response.status(201).location(`${this.path}/${activity?.id}`).send();
-  }
-
-  async getCollection(request: Request, response: Response) {
-    const activities = await this.activityService.getCollection();
-    response.json(activities);
-  }
-  async getById(request: Request<ParamsWithId>, response: Response) {
-    const { id } = request.params;
-
-    const activity = await this.activityService.getById(id);
-    if (!activity) {
-      throw new NotFound({
-        message: `Activity not found with id: \`${id}\``,
-        statusCode: 404,
-        code: 'ERR_NOT_FOUND',
-      });
-    }
-    response.json(activity);
-  }
-
-  async update(request: Request<ParamsWithId>, response: Response) {
-    const { id } = request.params;
-
-    const activity = await this.activityService.update(id, request.body);
-    response.status(200).json({ activity });
-  }
-
-  async delete(request: Request<ParamsWithId>, response: Response) {
-    const { id } = request.params;
-
-    await this.activityService.delete(id);
-    response.status(204).send();
+    super({
+      path: '/activities',
+      service: activityService,
+    });
   }
 }
 
--- src/controllers/car.controller.ts
+++ src/controllers/car.controller.ts
@@ -1,57 +1,12 @@
-import { NotFound } from '@/errors/index.js';
-import { ParamsWithId } from '@/interfaces/index.js';
 import { carService } from '@/services/index.js';
-import { Request, Response } from 'express';
+import { BaseController } from './base.controller.js';
 
-class CarController {
-  private carService: typeof carService;
-  private path: string;
-
+class CarController extends BaseController<typeof carService> {
   constructor() {
-    this.carService = carService;
-    this.path = '/cars';
-    this.getCollection = this.getCollection.bind(this);
-    this.getById = this.getById.bind(this);
-    this.create = this.create.bind(this);
-    this.update = this.update.bind(this);
-    this.delete = this.delete.bind(this);
-  }
-
-  async create(request: Request, response: Response) {
-    const [car] = await this.carService.create(request.body);
-    response.status(201).location(`${this.path}/${car?.id}`).send();
-  }
-
-  async getCollection(request: Request, response: Response) {
-    const cars = await this.carService.getCollection();
-    response.json(cars);
-  }
-  async getById(request: Request<ParamsWithId>, response: Response) {
-    const { id } = request.params;
-
-    const [car] = await this.carService.getById(id);
-    if (!car) {
-      throw new NotFound({
-        message: `Car not found with id: \`${id}\``,
-        statusCode: 404,
-        code: 'ERR_NOT_FOUND',
-      });
-    }
-    response.json(car);
-  }
-
-  async update(request: Request<ParamsWithId>, response: Response) {
-    const { id } = request.params;
-
-    const car = await this.carService.update(id, request.body);
-    response.status(200).json({ car });
-  }
-
-  async delete(request: Request<ParamsWithId>, response: Response) {
-    const { id } = request.params;
-
-    await this.carService.delete(id);
-    response.status(204).send();
+    super({
+      path: '/cars',
+      service: carService,
+    });
   }
 }
 
