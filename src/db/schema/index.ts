fc9be0e25cbc480d6ac15faec0d44b05a8ef9706
Yaya <yayazdiallo@outlook.fr>
2025-05-11 16:42:24 +0200

    feat: remove expense and revenue schemas

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,7 +1,5 @@
-export { usersTable } from '@/db/schema/user.schema.js';
-export { categoriesTable } from '@/db/schema/category.schema.js';
-export { carsTable } from '@/db/schema/car.schema.js';
-export { revenuesTable } from '@/db/schema/revenue.schema.js';
-export { transactionsTable } from '@/db/schema/transaction.schema.js';
-export { expensesTable } from '@/db/schema/expense.schema.js';
 export { activitiesTable } from '@/db/schema/activity.schema.js';
+export { carsTable } from '@/db/schema/car.schema.js';
+export { categoriesTable } from '@/db/schema/category.schema.js';
+export { transactionsTable } from '@/db/schema/transaction.schema.js';
+export { usersTable } from '@/db/schema/user.schema.js';
