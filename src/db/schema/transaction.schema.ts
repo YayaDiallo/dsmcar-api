ac5f3db987bf0835e4c3fcee1b820c0486d0e7f1
Yaya <yayazdiallo@outlook.fr>
2025-05-11 10:51:26 +0200

    feat: add transactions table schema and export it

---
+++ src/db/schema/transaction.schema.ts
@@ -0,0 +1,42 @@
+import { activitiesTable } from '@/db/schema/activity.schema.js';
+import { categoriesTable } from '@/db/schema/category.schema.js';
+import {
+  date,
+  numeric,
+  pgEnum,
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
+import {
+  createInsertSchema,
+  createSelectSchema,
+  createUpdateSchema,
+} from 'drizzle-zod';
+import { z } from 'zod';
+
+const kindEnum = pgEnum('kind', ['income', 'expense']);
+
+export const transactionsTable = pgTable('transactions', {
+  id: uuid().notNull().primaryKey().defaultRandom(),
+  activityId: uuid()
+    .references(() => activitiesTable.id)
+    .notNull(),
+  categoryId: uuid()
+    .references(() => categoriesTable.id)
+    .notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  kind: kindEnum().default('expense').notNull(),
+  amount: numeric({ precision: 12, scale: 2 }).notNull(),
+  date: date({ mode: 'string' }).notNull(),
+  createdAt: timestamp({ mode: 'string' }).notNull().defaultNow(),
+  updatedAt: timestamp({ mode: 'string' }).notNull().defaultNow(),
+});
+
+export const transactionSelectSchema = createSelectSchema(transactionsTable);
+export const transactionInsertSchema = createInsertSchema(transactionsTable);
+export const transactionUpdateSchema = createUpdateSchema(transactionsTable);
+
+export type TransactionInsertSchema = z.infer<typeof transactionSelectSchema>;
+export type TransactionSelectSchema = z.infer<typeof transactionSelectSchema>;
