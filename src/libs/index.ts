685d7d81c5ec3ff6738b2831d2cf1b3f48f6b2a5
Yaya <yayazdiallo@outlook.fr>
2025-06-11 23:20:24 +0200

    feat: implement JWT service for token generation and verification

---
+++ src/libs/index.ts
@@ -0,0 +1,1 @@
+export { jwtService } from '@/libs/jwt.js';
---
+++ src/libs/jwt.ts
@@ -0,0 +1,35 @@
+import { sign, SignOptions, verify, JwtPayload } from 'jsonwebtoken';
+
+class JWTService {
+  async generateToken({
+    payload,
+    expiresIn = '1h',
+  }: {
+    payload: { userId: string; email: string };
+    expiresIn: SignOptions['expiresIn'];
+  }): Promise<string> {
+    return new Promise((resolve, reject) => {
+      sign(payload, 'JWT_SECRET', { expiresIn }, (error, token) => {
+        if (error) return reject(error);
+        else return resolve(token as string);
+      });
+    });
+  }
+
+  async verifyToken({
+    token,
+    secret,
+  }: {
+    token: string;
+    secret: string;
+  }): Promise<JwtPayload | string> {
+    return new Promise((resolve, reject) => {
+      verify(token, secret, (error, decoded) => {
+        if (error) return reject(error);
+        else return resolve(decoded as JwtPayload | string);
+      });
+    });
+  }
+}
+
+export const jwtService = new JWTService();
