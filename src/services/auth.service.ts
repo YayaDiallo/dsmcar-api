4d7aaeada438cef2ce67420163bb61f8afcefe93
Yaya <yayazdiallo@outlook.fr>
2025-06-25 23:10:37 +0200

    feat: implement password hashing in register method and enhance login validation

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,6 +2,12 @@ import { UserInsertSchema, UserSelectSchema } from '@/db/schema/user.schema.js';
 import { Unauthenticated } from '@/errors/unauthenticated.js';
 import { userRepository } from '@/repositories/index.js';
 import { BaseService } from '@/services/base.service.js';
+import bcrypt from 'bcryptjs';
+
+const hashPassword = async (password: string): Promise<string> => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+};
 
 type UserRepositoryType = typeof userRepository;
 
@@ -15,13 +21,15 @@ class AuthService extends BaseService<
   }
 
   async register(data: UserInsertSchema) {
-    return this.repository.create(data);
+    const hashedPassword = await hashPassword(data.password);
+    return this.repository.create({ ...data, password: hashedPassword });
   }
 
-  // TODO: Implement proper password hashing and validation
   async login({ email, password }: { email: string; password: string }) {
     const user = await this.repository.getByEmail(email);
-    if (password === user?.password) return user;
+    const isPasswordValid =
+      user && (await bcrypt.compare(password, user.password));
+    if (user && isPasswordValid) return user;
 
     throw new Unauthenticated({
       message: 'Invalid email and/or password.',
