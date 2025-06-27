52f1f61562c44353ca3b82024177fcd3a1d6bc89
Yaya <yayazdiallo@outlook.fr>
2025-06-27 22:17:46 +0200

    feat: implement Redis service for caching and token management

---
+++ src/libs/redis.ts
@@ -0,0 +1,39 @@
+import { createClient } from 'redis';
+
+class RedisService {
+  private client: ReturnType<typeof createClient>;
+  constructor() {
+    this.client = createClient({ url: process.env.REDIS_URL });
+  }
+
+  connect() {
+    this.client
+      .on('error', (err) => {
+        console.error('Redis Client Error', err);
+      })
+      .connect();
+  }
+
+  setCache(key: string, value: string, ttl: number) {
+    return this.client.set(key, value, {
+      EX: ttl,
+      NX: true, // Only set if the key does not already exist
+    });
+  }
+  getCache(key: string) {
+    return this.client.get(key);
+  }
+  deleteCache(key: string) {
+    return this.client.del(key);
+  }
+
+  blacklistToken(token: string, ttl: number) {
+    return this.setCache(token, 'true', ttl);
+  }
+  async isTokenBlacklisted(token: string) {
+    const value = await this.getCache(token);
+    return value === 'true';
+  }
+}
+
+export const redisService = new RedisService();
