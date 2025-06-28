f73aca81e0f700ed7094b6f85a15502435b66cc9
Yaya <yayazdiallo@outlook.fr>
2025-06-29 00:33:47 +0200

    fix: update jwtLifeTime type to match SignOptions for better type safety

--- src/config/auth.config.ts
+++ src/config/auth.config.ts
@@ -1,4 +1,6 @@
+import { SignOptions } from 'jsonwebtoken';
+
 export const authConfig = {
   jwtSecret: process.env.JWT_SECRET || 'jwt_secret',
-  jwtLifeTime: process.env.JWT_LIFE_TIME || '1h',
+  jwtLifeTime: process.env.JWT_LIFE_TIME as SignOptions['expiresIn'],
 };
